0a1c8a6b3704f26b828974e36653d5b7aee0e379
benaidaraarsenemekondion <benaidara-arsene.MEKONDION@etu.univ-amu.fr>
2026-05-25 14:22:26 +0200

    implementation of encodage_positionnel

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,22 @@
-def encodage_positionnel( d_model, pos) :
+import numpy as np
+import torch
+
+def encodage_positionnel( d_model, positions) :
+   
+    position = np.arange(positions)[:, np.newaxis]
+    k = np.arange(d_model)[np.newaxis, :]
+    i = k // 2
+    
+    # initialize a matrix angle_rads of all the angles 
+    angle_rates = 1 / np.power(10000, (2 * i) / np.float32(d_model))
+    angle_rads = position * angle_rates
+  
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+  
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    
+    pos_encoding = angle_rads[np.newaxis, ...]
+    
+    return torch.cast(pos_encoding, dtype=torch.float32)
